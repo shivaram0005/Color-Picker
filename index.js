6fb0318f51e05780545b705421c86c862226f614
Shivaram0005 <shivaram8534@gmail.com>
2022-09-26 14:55:57 +0530

    initial commit

---
+++ index.js
@@ -0,0 +1,167 @@
+//firstredColor
+let hexaValueOne = document.getElementById("colorHexaOne");
+let colorOne = document.getElementById("color-one").addEventListener("click", function(){
+    hexaValueOne.style.display = "inline-block";
+    setTimeout(() => {
+        hexaValueOne.style.display = "none";
+    }, 3000);
+})
+
+
+//secondredColor
+let hexaValueTwo = document.getElementById("colorHexaTwo");
+let colorTwo = document.getElementById("color-two").addEventListener("click", function(){
+    hexaValueTwo.style.display = "inline-block";
+    setTimeout(() => {
+        hexaValueTwo.style.display = "none";
+    }, 3000);
+})
+
+//threeredColor
+let hexaValueThree = document.getElementById("colorHexaThree");
+let colorThree = document.getElementById("color-three").addEventListener("click", function(){
+    hexaValueThree.style.display = "inline-block";
+    setTimeout(() => {
+        hexaValueThree.style.display = "none";
+    }, 3000);
+})
+
+
+//forthredColor
+let hexaValueFour = document.getElementById("colorHexaFour");
+let colorHexaFour = document.getElementById("color-four").addEventListener("click", function(){
+    hexaValueFour.style.display = "inline-block";
+    setTimeout(() => {
+        hexaValueFour.style.display = "none";
+    }, 3000);
+})
+
+
+//fifthredColor
+let hexaValueFive = document.getElementById("colorHexaFive");
+let colorHexaFive = document.getElementById("color-five").addEventListener("click", function(){
+    hexaValueFive.style.display = "inline-block";
+    setTimeout(() => {
+        hexaValueFive.style.display = "none";
+    }, 3000);
+})
+
+//sixthredcolor
+let hexaValueSix = document.getElementById("colorHexaSix");
+let colorHexaSix = document.getElementById("color-six").addEventListener("click", function(){
+    hexaValueSix.style.display = "inline-block";
+    setTimeout(() => {
+        hexaValueSix.style.display = "none";
+    }, 3000);
+})
+
+//seventhbluecolor
+let hexaValueSeven = document.getElementById("colorHexaSeven");
+let colorHexaSeven = document.getElementById("color-seven").addEventListener("click", function(){
+    hexaValueSeven.style.display = "inline-block";
+    setTimeout(() => {
+        hexaValueSeven.style.display = "none";
+    }, 3000);
+})
+
+//eigthbluecolor
+let hexaValueEight = document.getElementById("colorHexaEight");
+let colorHexaEight = document.getElementById("color-eight").addEventListener("click", function(){
+    hexaValueEight.style.display = "inline-block";
+    setTimeout(() => {
+        hexaValueEight.style.display = "none";
+    }, 3000);
+})
+
+//ninebluecolor
+let hexaValueNine = document.getElementById("colorHexaNine");
+let colorHexaNine = document.getElementById("color-nine").addEventListener("click", function(){
+    hexaValueNine.style.display = "inline-block";
+    setTimeout(() => {
+        hexaValueNine.style.display = "none";
+    }, 3000);
+})
+
+
+//tenthbluecolor
+let hexaValueTen = document.getElementById("colorHexaTen");
+let colorHexaTen = document.getElementById("color-ten").addEventListener("click", function(){
+    hexaValueTen.style.display = "inline-block";
+    setTimeout(() => {
+        hexaValueTen.style.display = "none";
+    }, 3000);
+})
+
+
+//leventhbluecolor
+let hexaValueEleven = document.getElementById("colorHexaEleven");
+let colorHexaEleven = document.getElementById("color-eleven").addEventListener("click", function(){
+    hexaValueEleven.style.display = "inline-block";
+    setTimeout(() => {
+        hexaValueEleven.style.display = "none";
+    }, 3000);
+})
+
+
+//twelthcolor
+let hexaValueTwelve = document.getElementById("colorHexaTwelve");
+let colorHexaTwelve = document.getElementById("color-twelve").addEventListener("click", function(){
+    hexaValueTwelve.style.display = "inline-block";
+    setTimeout(() => {
+        hexaValueTwelve.style.display = "none";
+    }, 3000);
+})
+
+//thirteencolor
+let hexaValueThirteen = document.getElementById("colorHexaThirteen");
+let colorHexaThirteen = document.getElementById("color-thirteen").addEventListener("click", function(){
+    hexaValueThirteen.style.display = "inline-block";
+    setTimeout(() => {
+        hexaValueThirteen.style.display = "none";
+    }, 3000);
+})
+
+//fourteencolor
+let hexaValueFourteen = document.getElementById("colorHexaFourteen");
+let colorHexaFourteen = document.getElementById("color-fourteen").addEventListener("click", function(){
+    hexaValueFourteen.style.display = "inline-block";
+    setTimeout(() => {
+        hexaValueFourteen.style.display = "none";
+    }, 3000);
+})
+
+//fifteen
+let hexaValueFifteen = document.getElementById("colorHexaFifteen");
+let colorHexaFifteen = document.getElementById("color-fifteen").addEventListener("click", function(){
+    hexaValueFifteen.style.display = "inline-block";
+    setTimeout(() => {
+        hexaValueFifteen.style.display = "none";
+    }, 3000);
+})
+
+//sixteen
+let hexaValueSixteen = document.getElementById("colorHexaSixteen");
+let colorHexaSixteen = document.getElementById("color-sixteen").addEventListener("click", function(){
+    hexaValueSixteen.style.display = "inline-block";
+    setTimeout(() => {
+        hexaValueSixteen.style.display = "none";
+    }, 3000);
+})
+
+//seventeen
+let hexaValueSeventeen = document.getElementById("colorHexaSeventeen");
+let colorHexaSeventeen = document.getElementById("color-seventeen").addEventListener("click", function(){
+    hexaValueSeventeen.style.display = "inline-block";
+    setTimeout(() => {
+        hexaValueSeventeen.style.display = "none";
+    }, 3000);
+})
+
+//eighteen
+let hexaValueEighteen = document.getElementById("colorHexaEighteen");
+let colorHexaEighteen = document.getElementById("color-eighteen").addEventListener("click", function(){
+    hexaValueEighteen.style.display = "inline-block";
+    setTimeout(() => {
+        hexaValueEighteen.style.display = "none";
+    }, 3000);
+})
